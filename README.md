7de7ae2444989c436baa0d0b119cdb85d5139227
Kevin Boyle <papershack@gmail.com>
2017-11-04 20:53:21 -0700

    provide a README spec plan in accordance with BDD examples, and a blank slate for business logic

---
+++ README.md
@@ -0,0 +1,32 @@
+# _BeepBoop2_
+
+#### _BeepBoop2 is a resubmission_
+
+#### By _**Kevin Boyle**_
+_
+
+## Setup/Installation Requirements
+
+* _Go on to the page at https://lemurriot.github.io/beepboop/_
+
+
+## Specifications
+
+* _It can take a user inputted number and return a list of numbers from 0 to the user inputted number_
+  * _Example Input: 2_
+  * _Example Output: 012_
+* _It can take a user inputted number and return a list of numbers from 0 to the user inputted number, replacing any number that is a multiple of 3 with the word "pumpkin"_
+  * _Example Input: 3_
+  * _Example Output: 012pumpkin_
+* _It can take a user inputted number and return a list of numbers from 0 to the user inputted number, replacing any number that is a multiple of 4 with the word "spice"_
+  * _Example Input: 4_
+  * _Example Output: 012pumpkinspice_
+* _It can take a user inputted number and return a list of numbers from 0 to the user inputted number, replacing any number that is a multiple of 12 with the word "latte"_
+  * _Example Input: 12_
+  * _Example Output: 012pumpkinspice5pumpkin7spicepumpkin1011latte_
+
+
+
+
+
+Copyright (c) 2017 **_Kevin Boyle_**
---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+//Business End
+//end of Business Logic
+
+
+//Front End
+$(document).ready(function() {
+  $("form#numbersubmit").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#numnum").val());
+      if (isNaN(userInput)) {
+        alert("please enter valid number");
+      } else {
+    var finalAnswer = toBoop(userInput);
+    $(".card").show();
+    $("#result").append(finalAnswer);
+    }; //end of else statement
+  }); //end of .submit function
+}); //end of docready.function
+
+// Notes
+//
+// Kevin as we discussed you have a lot of great looking code, but I'd like the opportunity to look at your process from the beginning... specifically using specs as your guide.
+//
+// To that end I'd like you to:
+//
+// Copy your repo for me and erase your business logic from your js/scripts.js document.
+//
+// Create a new readme that includes your name and your specs. Doesn't have to include any other info.
+//
+// Start over with your business logic using this prompt:
+//
+// Write a program that takes a user inputted number and returns a list of numbers from 0 to the inputted number with the following exceptions:
+// 1. Any number divisible by 3 should be replaced with "pumpkin".
+// 2. Any number divisible by 4 should be replaced with "spice".
+// 3. Any number divisible by 12 should be replaced with "latte".
+// Please include three commits: 1. Commit with empty business logic and specs written for the prompt above. 2. Commit after writing the code to pass the first spec. 3. Commit after writing code to pass the second spec.
+//
+// Please make sure to be incremental and make sure to use only the resources available to you given our academic honesty policy. Thanks for you hard work here! Looking forward to seeing your resub!
